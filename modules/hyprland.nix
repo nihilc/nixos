1aa895b661d6e72c2ccc5a78eb458a55ad1800ed
Christian Cardenas <nihilist.cc@gmail.com>
2025-08-11 01:53:45 -0500

    add more hyprland programs

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -15,14 +15,17 @@
     # System
     wl-clipboard # cliboard
     cliphist # clipboard history manager
-    dunst # notification manager
+    swaynotificationcenter # notification manager
     libnotify # notification lib
     brightnessctl # brightness
     gammastep # brightness / night mode
     playerctl # media control
     pulsemixer # audio control
+    networkmanagerapplet # network control
     hyprshot # Screenshots
     hyprpicker # color picker
+    hypridle # idle control
+    hyprlock # block screen
   ];
 
   fonts.packages = with pkgs; [
